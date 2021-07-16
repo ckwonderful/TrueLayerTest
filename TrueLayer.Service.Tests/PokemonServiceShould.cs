aa9a75f83e71e51899845fe4c58c1c25e74950fa
Carol Kelly <carol_irish@hotmail.com>
2021-07-16 20:31:35 +0100

    first test passing, mocked http client

--- TrueLayer.Service.Tests/PokemonServiceShould.cs
+++ TrueLayer.Service.Tests/PokemonServiceShould.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
+using Moq;
 using NUnit.Framework;
 
 namespace TrueLayer.Service.Tests
@@ -10,14 +13,28 @@ namespace TrueLayer.Service.Tests
         [Test]
         public void ReturnBasicPokemonInformationForAValidName()
         {
+            var pokemonSpecies = new PokemonSpecies
+            {
+                name = "mewtwo",
+                habitat = new Habitat { name = "home" },
+                is_legendary = true,
+                flavor_text_entries = new List<FlavorText>
+                    {new FlavorText {flavor_text = "flavor1", language = "en"}}
+            };
+
+            var httpService = new Mock<IHttpService>();
+            httpService.Setup(x => x.Get<PokemonSpecies>("mewtwo"))
+                .Returns(pokemonSpecies);
+
             var expectedResult = new Pokemon
             {
-                Name = "mewtwo",
-                Description = "something",
-                Habitat = "home",
-                IsLegendary = true
+                Name = pokemonSpecies.name,
+                Description = pokemonSpecies.flavor_text_entries.First().flavor_text,
+                Habitat = pokemonSpecies.habitat.name,
+                IsLegendary = pokemonSpecies.is_legendary
             };
-            var sut = new PokemonService();
+            var sut = new PokemonService(httpService.Object);
+
             sut.GetPokemonBasicDetails("mewtwo").Should().BeEquivalentTo(expectedResult);
         }
     }
@@ -32,10 +49,48 @@ namespace TrueLayer.Service.Tests
 
     public class PokemonService : IPokemonService
     {
+        private readonly IHttpService _httpService;
+
+        public PokemonService(IHttpService httpService)
+        {
+            _httpService = httpService;
+        }
+
         public Pokemon GetPokemonBasicDetails(string mewtwo)
         {
-            throw new NotImplementedException();
+            var pokemonSpeciesDetails = _httpService.Get<PokemonSpecies>(mewtwo);
+            return new Pokemon
+            {
+                Name = pokemonSpeciesDetails.name,
+                Description = pokemonSpeciesDetails.flavor_text_entries.FirstOrDefault(x => x.language == "en").flavor_text,
+                Habitat = pokemonSpeciesDetails.habitat.name,
+                IsLegendary = pokemonSpeciesDetails.is_legendary
+            };
         }
+    }
+
+    public class PokemonSpecies
+    {
+        public string name;
+        public List<FlavorText> flavor_text_entries;
+        public Habitat habitat;
+        public bool is_legendary;
+    }
+
+    public class Habitat
+    {
+        public string name;
+    }
+
+    public class FlavorText
+    {
+        public string language;
+        public string flavor_text;
+    }
+
+    public interface IHttpService
+    {
+        T Get<T>(string mewtwo);
     }
 
     public interface IPokemonService
