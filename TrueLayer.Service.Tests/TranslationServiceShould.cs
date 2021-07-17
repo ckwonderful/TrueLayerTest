673d0c78af63c1732e0ad21a44ed02e4bcabb123
Carol Kelly <carol_irish@hotmail.com>
2021-07-17 10:50:20 +0100

    add translation service test failing

---
+++ TrueLayer.Service.Tests/TranslationServiceShould.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TrueLayer.Service.Tests
+{
+    [TestFixture]
+    public class TranslationServiceShould
+    {
+        [Test]
+        public async Task TranslateTheGivenText()
+        {
+            var sut = new TranslationService();
+            (await sut.Translate("translate-me")).Should().Be("translate-me-translated");
+        }
+    }
+
+    public class TranslationService: ITranslationService
+    {
+        public async Task<string> Translate(string translateMe)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface ITranslationService
+    {
+        Task<string> Translate(string translateMe);
+    }
+}
